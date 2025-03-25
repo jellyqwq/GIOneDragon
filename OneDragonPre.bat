a0c46bb5cd184bfa2108081fadad7b4a092132f3
SweetJellyQwQ <577430840@qq.com>
2025-03-25 18:12:24 +0800

    脚本异常修复

--- OneDragonPre.bat
+++ OneDragonPre.bat
@@ -4,8 +4,10 @@ chcp 65001 >nul
 
 :: 该脚本为 March7th Launcher.exe 的子进程, March7th 被终止后该脚本也会被终止
 :: 因此需要单独调用一个 bat 来启动 BetterGI
-start "" "D:\Projects\GIOneDragon\OneDragon.bat"
+start "" "D:\Program Files\BetterGI\OneDragon-v1.0.0.bat"
 echo 批处理脚本继续执行...
+echo wait 5s
+timeout /t 5 /nobreak >nul
 
 :: 检查 March7th Launcher.exe
 tasklist | findstr /I "March7th Launcher.exe" >nul
