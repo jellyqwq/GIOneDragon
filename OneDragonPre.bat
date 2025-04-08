9e426322125d5c0d99b95579da562105fad572f7
jellyqwq <577430840@qq.com>
2025-04-09 00:23:31 +0800

    注释标准化，修复终止后异常输出

--- OneDragonPre.bat
+++ OneDragonPre.bat
@@ -2,14 +2,13 @@ echo off
 setlocal enabledelayedexpansion
 chcp 65001 >nul
 
-:: 该脚本为 March7th Launcher.exe 的子进程, March7th 被终止后该脚本也会被终止
-:: 因此需要单独调用一个 bat 来启动 BetterGI
-start "" "D:\Program Files\BetterGI\OneDragon-v1.0.0.bat"
+@REM 该脚本为 March7th Launcher.exe 的子进程, March7th 被终止后该脚本也会被终止
+@REM 因此需要单独调用一个 bat 来启动 BetterGI
+@REM 使用 cmd /c 启动 bat 以解决 OneDragon.bat 运行完后不能退出窗口
+start "OneDragon" cmd /c "C:\Program Files\March7thAssistant\OneDragon.bat"
 echo 批处理脚本继续执行...
-echo wait 5s
-timeout /t 5 /nobreak >nul
 
-:: 检查 March7th Launcher.exe
+@REM 检查 March7th Launcher.exe
 tasklist | findstr /I "March7th Launcher.exe" >nul
 if %errorlevel%==0 (
     taskkill /F /IM "March7th Launcher.exe"
